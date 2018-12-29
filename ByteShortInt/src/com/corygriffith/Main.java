d4a2a52fb3e141b19daefe501b56cbfa0db13d36
coryrgriffith <corygriffith9@gmail.com>
2018-12-29 14:48:28 -0600

    add float and double examples

--- ByteShortInt/src/com/corygriffith/Main.java
+++ ByteShortInt/src/com/corygriffith/Main.java
@@ -3,9 +3,18 @@ package com.corygriffith;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Integer has width of 32
+
+
         int myValue = 10000;
         int myMinValue = -2_147_483_648;
         int myMaxValue = 2_147_483_647;
+    //byte has width of 8
+        byte myByteValue = 127;
+        byte myMinByte = -128;
+    //short has length of 16
+        short myShortValue = 32767;
+    //long has width of 64
+        long myLongValue = 9_223_372_036_854_775_807L;
     }
 }
---
+++ FloatAndDouble/src/com/corygriffith/Main.java
@@ -0,0 +1,12 @@
+package com.corygriffith;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+        System.out.println();
+    }
+}
