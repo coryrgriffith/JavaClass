3971d157bcd12b6e107b94c4403c547a6469ca89
coryrgriffith <corygriffith9@gmail.com>
2018-12-29 15:17:08 -0600

    add string examples

---
+++ Strings/src/com/corygriffith/Main.java
@@ -0,0 +1,25 @@
+package com.corygriffith;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 8 primitive datatypes
+	    // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+
+        String myString = "This is a string";
+
+        System.out.println("myString is equal to " + myString);
+
+        myString = myString + ", and this is more.";
+
+        System.out.println("myString is equal to " + myString);
+
+    }
+}
