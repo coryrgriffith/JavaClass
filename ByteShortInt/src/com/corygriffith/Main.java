60108028fc4f6f63ce9f6c728cfaf03ec04f2e51
coryrgriffith <corygriffith9@gmail.com>
2018-12-29 14:28:13 -0600

    add integer examples

---
+++ ByteShortInt/src/com/corygriffith/Main.java
@@ -0,0 +1,11 @@
+package com.corygriffith;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myValue = 10000;
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+    }
+}
